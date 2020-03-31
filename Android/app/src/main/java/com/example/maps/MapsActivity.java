2f05d36a4b6ebb759873c795a2fccfbe6c917f29
katinas15 <laimonas.janutenas@gmail.com>
2020-03-31 22:24:53 +0300

    update coords

--- Android/app/src/main/java/com/example/maps/MapsActivity.java
+++ Android/app/src/main/java/com/example/maps/MapsActivity.java
@@ -88,12 +88,59 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         new LatLng(54.689527, 25.320268),
                         new LatLng(54.688566, 25.318723),
                         new LatLng(54.687208, 25.317532)
-
                         )
+                .strokeColor(Color.argb(150,100,100,100))
+                .fillColor(Color.argb(150,100,100,100));
+
+
+        PolygonOptions rectOptionsA = new PolygonOptions()
+                .add(new LatLng(54.694380, 25.302390),
+                        new LatLng(54.693979, 25.303548),
+                        new LatLng(54.693836, 25.304170),
+                        new LatLng(54.693811, 25.305327),
+                        new LatLng(54.693320, 25.305845),
+                        new LatLng(54.692977, 25.305054),
+                        new LatLng(54.692258, 25.305376),
+                        new LatLng(54.691382, 25.305139),
+                        new LatLng(54.690330, 25.304492),
+                        new LatLng(54.689260, 25.304360),
+                        new LatLng(54.688472, 25.304627),
+                        new LatLng(54.688645, 25.305128),
+                        new LatLng(54.688502, 25.306205),
+                        new LatLng(54.688505, 25.308184),
+                        new LatLng(54.688857, 25.313411),
+                        new LatLng(54.688750, 25.315077),
+                        new LatLng(54.687087, 25.317370),
+                        new LatLng(54.684482, 25.320734),
+                        new LatLng(54.683587, 25.318989),
+                        new LatLng(54.682413, 25.314827),
+                        new LatLng(54.685649, 25.307743),
+                        new LatLng(54.688100, 25.304962),
+                        new LatLng(54.686376, 25.302551),
+                        new LatLng(54.684692, 25.301862),
+                        new LatLng(54.682041, 25.298590),
+                        new LatLng(54.683179, 25.299196),
+                        new LatLng(54.683641, 25.299055),
+                        new LatLng(54.684590, 25.298030),
+                        new LatLng(54.685083, 25.296312),
+                        new LatLng(54.685196, 25.296195),
+                        new LatLng(54.685876, 25.295915),
+                        new LatLng(54.686083, 25.295751),
+                        new LatLng(54.686515, 25.295132),
+                        new LatLng(54.686904, 25.294180),
+                        new LatLng(54.688635, 25.292866),
+                        new LatLng(54.688958, 25.292771),
+                        new LatLng(54.689307, 25.293664),
+                        new LatLng(54.689724, 25.294385),
+                        new LatLng(54.691051, 25.296194),
+                        new LatLng(54.691428, 25.296795),
+                        new LatLng(54.692583, 25.299706)
+                    )
                 .strokeColor(Color.argb(150,100,100,100))
                 .fillColor(Color.argb(150,100,100,100));
 
 // Get back the mutable Polygon
         Polygon polygon = mMap.addPolygon(rectOptions);
+        Polygon polygonA = mMap.addPolygon(rectOptionsA);
     }
 }
