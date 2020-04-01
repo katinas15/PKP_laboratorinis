adaf0f8c0f6bf466bfbc9f3e28d7013b68810826
Edgar Kaziukonis <kaziukonis.edgar@gmail.com>
2020-04-02 02:38:46 +0300

    init

---
+++ API/src/models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true
+    }
+})
+
+module.exports = User
---
+++ API/src/routers/user.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const User = require('../models/user')
+const router = new express.Router()
+
+var bcrypt = require('bcrypt');
+const saltRounds = 8;
+var jwt = require('jsonwebtoken');
+const jwtKey = 'KLMNjPrSt98'
+const jwtExpireSeconds = 10000;
+
+router.post('/register', async (req, res) => {
+    let { email, password } = req.body
+    try {
+        let hashedPassword = await bcrypt.hash(password, saltRounds)
+        const user = new User({ password: hashedPassword, email: email })
+        const token = jwt.sign({ email }, jwtKey, {
+            algorithm: 'HS256',
+            expiresIn: jwtExpireSeconds,
+        })
+        await user.save()
+        res.send(JSON.stringify({ accessToken: token }));
+    } catch (e) {
+        console.log()
+        res.status(400).send(e)
+    }
+})
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email: email })
+
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password)
+        if (passwordMatch) {
+            const token = jwt.sign({ email }, jwtKey, {
+                algorithm: 'HS256',
+                expiresIn: jwtExpireSeconds,
+            })
+            res.send(JSON.stringify({ accessToken: token }));
+            res.end()
+        } else {
+            return res.status(401).end();
+        }
+
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+module.exports = router
