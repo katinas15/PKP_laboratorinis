b2c7ef0a0b8db086d74c4f5b6212b6a4c83b2f30
Laimonas Janutėnas <41838906+katinas15@users.noreply.github.com>
2020-04-03 19:05:01 +0300

    Update JsonZoneParser.java

--- JsonZoneParser.java
+++ JsonZoneParser.java
@@ -12,23 +12,29 @@ public class Main {
             File myObj = new File("C:\\Users\\l\\IdeaProjects\\untitled\\src\\com\\company\\data.txt");
             FileWriter myWriter = new FileWriter("cords.txt");
             Scanner myReader = new Scanner(myObj);
-            String name = "antra Zona";
-            String color = "red";
+            String name = "pirma Zona";
+            String color = "zalia";
             myWriter.write(
                     "{\n" +
                             "\t\"name\": \"" + name + "\",\n" +
                             "\t\"color\": \"" + color + "\",\n" +
                     "\t\"bounds\": [\n"
             );
+            boolean firstTime = true;
             while (myReader.hasNextLine()) {
+                if(firstTime){
+                    firstTime = !firstTime;
+                } else {
+                    myWriter.write(",\n");
+                }
                 String data = myReader.nextLine();
                 String[] parts = data.split(", ");
                 myWriter.write("\t\t{\n" +
                         "\t\t\t\"x\": " + parts[0] + ",\n" +
                         "\t\t\t\"y\": " + parts[1] + "\n" +
-                        "\t\t},\n");
+                        "\t\t}");
             }
-            myWriter.write("\t\t}\n\t]\n}");
+            myWriter.write("\n\t]\n}");
             myReader.close();
             myWriter.close();
         } catch (FileNotFoundException e) {
