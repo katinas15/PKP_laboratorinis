1997f7fca11609673c09504013eb9f1b8c1a64af
katinas15 <laimonas.janutenas@gmail.com>
2020-04-05 19:28:15 +0300

    rodomas naudotojo location

--- Android/app/src/main/java/com/example/maps/MapsActivity.java
+++ Android/app/src/main/java/com/example/maps/MapsActivity.java
@@ -1,21 +1,38 @@
 package com.example.maps;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.google.android.gms.common.internal.Constants;
+
+import com.google.android.gms.*;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polygon;
 import com.google.android.gms.maps.model.PolygonOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
@@ -30,32 +47,70 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     List<Zone> zones;
     Map<String, Integer> colors = new HashMap<String, Integer>();
 
+    LocationManager locationManager;
+    LocationListener locationListener;
+    static LatLng userLocation;
+    LatLng clickLocation;
+    String provider;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        provider = locationManager.getBestProvider(new Criteria(), false);
+
+        checkLocationPermission();
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            if (mMap != null) {
+                mMap.setMyLocationEnabled(true);
+                mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+                    @Override
+                    public void onMyLocationChange(Location location) {
+                        userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                    }
+                });
+                Toast.makeText(MapsActivity.this, "rodomas location", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            Toast.makeText(MapsActivity.this, "returned", Toast.LENGTH_LONG).show();
+            return;
+        }
+
         colors.put("raudona", Color.argb(100,252, 40, 3));
         colors.put("zalia", Color.argb(100,15, 219, 66));
         colors.put("geltona", Color.argb(100,252, 219, 3));
         colors.put("melyna", Color.argb(100,3, 119, 252));
 
-        mMap = googleMap;
-
         GetMainZones get = new GetMainZones();
         get.execute();
 
-        mMap.setMinZoomPreference(14.0f);
-        LatLng vilnius = new LatLng(54.694380, 25.302390);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(vilnius));
+        mMap.setMinZoomPreference(10.0f);
+
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                clickLocation = latLng;
+                mMap.addMarker(new MarkerOptions()
+                        .position(clickLocation)
+                        .title("Hello world"));
+            }
+        });
 
 
     }
@@ -96,6 +151,81 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 mMap.addPolygon(rectangle);
             }
 
+
+        }
+    }
+
+
+
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(this)
+                        .setTitle("hello")
+                        .setMessage("give permission ok ?")
+                        .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(MapsActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        MY_PERMISSIONS_REQUEST_LOCATION);
+                            }
+                        })
+                        .create()
+                        .show();
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+
+                        //Request location updates:
+                        locationManager.requestLocationUpdates(provider, 400, 1, (LocationListener) this);
+                    }
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+
+                }
+                return;
+            }
 
         }
     }
