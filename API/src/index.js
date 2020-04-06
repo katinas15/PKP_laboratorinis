eb0e5d8bc8dc92e9c5a5a462fc8cd91f3ba99aff
katinas15 <laimonas.janutenas@gmail.com>
2020-04-06 20:47:09 +0300

    sutvarkyti user zone routes

--- API/src/index.js
+++ API/src/index.js
@@ -3,6 +3,7 @@ require('./db/mongoose')
 const userRouter = require('./routers/user')
 const mainZoneRouter = require('./routers/mainZone')
 const ratingRouter = require('./routers/rating')
+const userZoneRouter = require('./routers/userZone')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -11,6 +12,8 @@ app.use(express.json())
 app.use(userRouter)
 app.use(mainZoneRouter)
 app.use(ratingRouter)
+app.use(userZoneRouter)
+
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
