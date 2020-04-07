c13538027ad321b68b77a1df5e8d72a054a62696
Edgar Kaziukonis <kaziukonis.edgar@gmail.com>
2020-04-07 21:17:55 +0300

    some cleanup

--- API/src/routers/user.js
+++ API/src/routers/user.js
@@ -4,19 +4,12 @@ const router = new express.Router()
 
 var bcrypt = require('bcrypt');
 const saltRounds = 8;
-var jwt = require('jsonwebtoken');
-const jwtKey = 'KLMNjPrSt98'
-const jwtExpireSeconds = 10000;
 
 router.post('/register', async (req, res) => {
     let { email, password } = req.body
     try {
         let hashedPassword = await bcrypt.hash(password, saltRounds)
         const user = new User({ password: hashedPassword, email: email })
-        const token = jwt.sign({ email }, jwtKey, {
-            algorithm: 'HS256',
-            expiresIn: jwtExpireSeconds,
-        })
         await user.save()
         res.send(user);
     } catch (e) {
@@ -36,10 +29,6 @@ router.post('/login', async (req, res) => {
 
         const passwordMatch = await bcrypt.compare(password, user.password)
         if (passwordMatch) {
-            const token = jwt.sign({ email }, jwtKey, {
-                algorithm: 'HS256',
-                expiresIn: jwtExpireSeconds,
-            })
             res.send(user);
             res.end()
         } else {
