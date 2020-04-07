43f91ec37cc15b76a21fe78c8a4b042f4dd82559
Laimonas Janutėnas <41838906+katinas15@users.noreply.github.com>
2020-04-07 21:50:41 +0300

    Update rating.js

--- API/src/routers/rating.js
+++ API/src/routers/rating.js
@@ -5,7 +5,7 @@ const router = new express.Router()
 var jwt = require('jsonwebtoken');
 const jwtKey = 'KLMNjPrSt98'
 
-router.post('/mainZone/:id/rate', async (req, res) => {
+router.post('/userZone/:id/rate', async (req, res) => {
     const _id = req.params.id
     const { userId, rating } = req.body
     const zoneRating = new Rating({ userId: userId, userZoneId: _id, rating: rating })
@@ -15,6 +15,20 @@ router.post('/mainZone/:id/rate', async (req, res) => {
         res.status(201).send(zoneRating)
     } catch (e) {
         res.status(400).send(e)
+    }
+})
+
+router.get('/userZone/:id', async (req, res) => {
+    const _id = req.params.id
+    try {
+        const userZone = await UserZone.findById(_id, req.body)
+        if (!userZone) {
+            return res.status(404).send()
+        }
+        res.send(userZone)
+    } catch (e) {
+        console.log(e);
+        res.status(500).send()
     }
 })
 
